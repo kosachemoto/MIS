e507fad5dfa8337968150c2b23eb6bd354fcb50b
Nikita Kosachev <kosachev.a.nikita@gmail.com>
2018-03-28 01:13:33 +0300

    few functions was added into lab-1

--- lab-1/index.js
+++ lab-1/index.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('hello world');
+const flowOfCustomers  = 1.5; // Поток покупателей ( чел. / мин. )
+const serviceTime = 2; // Среднее время обслуживание клиента ( чел. / мин. )
+
+function getFlowIntencity(serviceTime) {
+  return 1 / serviceTime;
+}
+
+function getUtilizationRateOfQS(flowOfCustores, flowIntencity) {
+  return flowOfCustores / flowIntencity;
+}
+
+const flowIntencity = getFlowIntencity(serviceTime);
+const utilizationRateOfQS = getUtilizationRateOfQS(flowOfCustomers, flowIntencity);
+
+console.log('# LAB-1');
+console.log('Интенсивность потока покупателей ( в минуту ): ' + flowIntencity);
+console.log('Коэффициент использования СМО: ' + utilizationRateOfQS);
